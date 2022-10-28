7a0af9b3f6923a9544db5a2fc54537dde1fe0f36
broniuu <86781217+broniuu@users.noreply.github.com>
2022-10-28 20:37:03 +0200

    lab4

--- WR_lab2/Assets/Scripts/PlayerCollisions.cs
+++ WR_lab2/Assets/Scripts/PlayerCollisions.cs
@@ -4,30 +4,51 @@ using UnityEngine;
 
 public class PlayerCollisions : MonoBehaviour
 {
-    bool doorIsOpen = false;
-    float doorTimer = 0.0f;
-    public float doorOpenTime = 3.0f;
-    public AudioClip doorOpenSound;
-    public AudioClip doorShutSound;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // bool doorIsOpen = false;
+    // float doorTimer = 0.0f;
+    // public float doorOpenTime = 3.0f;
+    // public AudioClip doorOpenSound;
+    // public AudioClip doorShutSound;
+    GameObject currentDoor;
 
-    // Update is called once per frame
+    // // Start is called before the first frame update
+    // void Start() { }
+
+    // // Update is called once per frame
     void Update()
     {
-        
-    }
-    void OnControllerColliderHit(ControllerColliderHit hit){
-        if(hit.gameObject.tag == "playerDoor" && doorIsOpen == false){
-            OpenDoor(hit.gameObject);
+        // if (doorIsOpen)
+        // {
+        //     doorTimer += Time.deltaTime;
+        //     if (doorTimer > doorOpenTime)
+        //     {
+        //         Door(doorShutSound, false, "doorshut", currentDoor);
+        //         doorTimer = 0.0f;
+        //     }
+        // }
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 3))
+        {
+            if (hit.collider.gameObject.tag == "playerDoor")
+            {
+                currentDoor = hit.collider.gameObject;
+                currentDoor.SendMessage("DoorCheck");
+            }
         }
     }
-    void OpenDoor(GameObject door){
-        doorIsOpen = true;
-        door.GetComponent<AudioSource>().PlayOneShot(doorOpenSound);
-        door.transform.parent.GetComponent<Animation>().Play("dooropen");
-    }
+
+    // // void OnControllerColliderHit(ControllerColliderHit hit)
+    // // {
+    // //     if (hit.gameObject.tag == "playerDoor" && doorIsOpen == false)
+    // //     {
+    // //         currentDoor = hit.gameObject;
+    // //         Door(doorOpenSound, true, "dooropen", currentDoor);
+    // //     }
+    // // }
+
+    // void Door(AudioClip aClip, bool openCheck, string animName, GameObject thisDoor) {
+    //     doorIsOpen = openCheck;
+    //     thisDoor.GetComponent<AudioSource>().PlayOneShot(aClip);
+    //     thisDoor.transform.parent.GetComponent<Animation>().Play(animName);
+    // }
 }
