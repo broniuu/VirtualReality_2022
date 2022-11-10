2a704e594263bbd7dbd66eb0b185be9043560e22
broniuu <86781217+broniuu@users.noreply.github.com>
2022-11-10 16:24:12 +0100

    lab6

--- WR_lab2/Assets/Scripts/Inventory.cs
+++ WR_lab2/Assets/Scripts/Inventory.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
     public static int charge;
+    public Texture2D[] hudCharge;
+    public RawImage chargeHudGUI;
+
+    // Generator
+    public Texture2D[] meterCharge;
+    public Renderer meter;
     public AudioClip collectSound;
 
     // Start is called before the first frame update
@@ -18,7 +25,18 @@ public class Inventory : MonoBehaviour
 
     void CellPickup()
     {
+        HUDon();
         AudioSource.PlayClipAtPoint(collectSound, transform.position);
         charge++;
+        chargeHudGUI.texture = hudCharge[charge];
+        meter.material.mainTexture = meterCharge[charge];
+    }
+
+    void HUDon()
+    {
+        if (!chargeHudGUI.enabled)
+        {
+            chargeHudGUI.enabled = true;
+        }
     }
 }
